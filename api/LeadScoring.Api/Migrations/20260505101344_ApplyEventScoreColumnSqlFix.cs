d42ebff022cc49a86e95a7f1b99833dffd944722
AmarScadea <amar.maurya@scadea.com>
2026-05-05 15:46:34 +0530

    updated the column migration

---
+++ api/LeadScoring.Api/Migrations/20260505101344_ApplyEventScoreColumnSqlFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeadScoring.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyEventScoreColumnSqlFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER TABLE "Events"
+                ADD COLUMN IF NOT EXISTS "EventScore" integer NOT NULL DEFAULT 0;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                ALTER TABLE "Events"
+                DROP COLUMN IF EXISTS "EventScore";
+                """);
+        }
+    }
+}
